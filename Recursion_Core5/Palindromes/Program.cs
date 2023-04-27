0ccab53ebd9fc687974482ef3ddb9776d3275c07
Joel Southall <southallj@my.lanecc.edu>
2023-04-27 02:20:35 -0700

    Implement first state of Palindrome

--- Recursion_Core5/Palindromes/Program.cs
+++ Recursion_Core5/Palindromes/Program.cs
@@ -1,12 +1,54 @@
-﻿using System;
+﻿/*
+ * Joel Southall
+ * April 27, 2023
+ * 
+ * 
+ * Recursive method IsPalindrome to mark if string is true or false
+ * Will need to remove whitespace, punctuation and capitalization
+ * Base case is going to be if length of the string is 0 or 1, which is true.
+ * Compare last and first characters of string. If not equal then IsPalindrome = false.
+ * IsPalindrom method should be called recursively.
+ * User Input is a word or phrase entered.
+ * Output is whether or not the input is a palindrome. 
+ * 
+ */
 
-namespace Palindromes
+using System;
+using System.Linq;
+
+namespace Palindrome
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.Write("Enter word or phrase: ");
+            string text = Console.ReadLine();
+
+            if (IsPalindrome(text))
+            {
+                Console.WriteLine("The text is a palindrome!");
+            }
+            else
+            {
+                Console.WriteLine("The text is not a palindrome.");
+            }
+
+            Console.ReadLine();
+        }
+
+        static bool IsPalindrome(string s)
+        {
+            // Base case: if the string is empty or has one character, it's a palindrome
+            if (s.Length < 2)
+            {
+                return true;
+            }
+            // To do, recursive case
+            else
+            {
+                return false;
+            }
         }
     }
 }
