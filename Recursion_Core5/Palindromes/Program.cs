065f4be927c7c9ac7829d690bb0fb9cf86fb1cc8
Joel Southall <southallj@my.lanecc.edu>
2023-05-07 19:26:37 -0700

    For Palindrome program, add eval of multiple phrases and playagain feature

--- Recursion_Core5/Palindromes/Program.cs
+++ Recursion_Core5/Palindromes/Program.cs
@@ -15,6 +15,14 @@
  * User Input is a word or phrase entered.
  * Output is whether or not the input is a palindrome. 
  * 
+ * PSUEDOCODE FOR ADDED CALCULATION OF MULTIPLE PHRASES AND PLAYAGAIN FEATURE
+ * 1. Prompt the user to enter words and phrases to evaluate (separated by commas)
+ * 2. Split the input string into array of strings using comma as a delimiter
+ * 3. For each string in the array, check if palindrome using the IsPalindrome function
+ * 4. Print a message indicating whether each string is a palindrome or not
+ * 5. Ask the user if they want to play again
+ * 6. If yes, go to step 1; if no, exit the program
+ * 
  */
 
 using System;
@@ -26,24 +34,35 @@ namespace Palindrome
     {
         static void Main(string[] args)
         {
-            Console.Write("Enter word or phrase: ");
-            string text = Console.ReadLine();
+            bool playAgain = true;
 
-            // Remove all non-alphanumeric characters and convert to lowercase
-            string cleanText = new string(text.ToLower().ToCharArray()
-                                            .Where(c => Char.IsLetterOrDigit(c))
-                                            .ToArray());
-
-            if (IsPalindrome(cleanText))
+            while (playAgain)
             {
-                Console.WriteLine("The text is a palindrome!");
-            }
-            else
-            {
-                Console.WriteLine("The text is not a palindrome.");
-            }
+                Console.WriteLine("Enter one or more words or phrases (separated by commas):");
+                string input = Console.ReadLine();
 
-            Console.ReadLine();
+                string[] texts = input.Split(',');
+
+                foreach (string text in texts)
+                {
+                    string cleanText = new string(text.ToLower().ToCharArray()
+                                                    .Where(c => Char.IsLetterOrDigit(c))
+                                                    .ToArray());
+
+                    if (IsPalindrome(cleanText))
+                    {
+                        Console.WriteLine("The text \"" + text + "\" is a palindrome!");
+                    }
+                    else
+                    {
+                        Console.WriteLine("The text \"" + text + "\" is not a palindrome.");
+                    }
+                }
+
+                Console.Write("Play again? (Y/N): ");
+                string answer = Console.ReadLine().ToUpper();
+                playAgain = (answer == "Y");
+            }
         }
 
         static bool IsPalindrome(string s)
